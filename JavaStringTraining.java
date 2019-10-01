bdb6919ad1a06fb2e9cfcd26d6c6d597389575de
[Bartosz Bala] <[bala.bartosz@gmail.com]>
2019-10-01 22:40:54 +0200

    commit after first run ogf program

--- JavaStringTraining.java
+++ JavaStringTraining.java
@@ -1,24 +1,39 @@
+import java.util.Scanner;
+
 public class JavaStringTraining{
 
-public String swapCase(String initialValue)
+public static  String swapCase(String initialValue)
 {
-int length = initialValue.length;
+int length = initialValue.length();
 
 char[] charArrays = initialValue.toCharArray();
 
 for(int i=0;i<length;i++)
-{if(Character.isLowerCase(charArrays[i];)
+{if(Character.isLowerCase(charArrays[i]))
 {charArrays[i]=Character.toUpperCase(charArrays[i]);}
 else
-{charArrays[i]=Character.toLowerCase(charArrays[i];}
+{charArrays[i]=Character.toLowerCase(charArrays[i]);}
 
 
-initialValue = new String(charArrays);
+
+}
+initialValue = String.copyValueOf(charArrays);
+return initialValue;
 }
 
-public static void main(String args[])
+public static void main(String[] args)
 
-{}
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("wpisz dowolny tekst");
+String initialValue = sc.nextLine();
+System.out.println("wartosc zmieniona to");
+System.out.println(swapCase(initialValue));
+
+
+	
+	
+}
 
 
 
